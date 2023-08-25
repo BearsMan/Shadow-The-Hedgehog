095bbba0b0653dc5c4c0d25ae57ccf9be2da1ab4
Darius Ronkowitz <dronkowitz@yahoo.com>
2023-08-25 06:52:33 -0400

    Updated PlayerMovement.cs
    Changed ```rb``` variable to ```Rigidbody```, and changed ```MovementController``` function and void to ```Move()```

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-    public Rigidbody rb;
+    public Rigidbody body;
     public float jumpForce;
     public float groundCheckDistance = 0.1f;
     public LayerMask groundMask;
@@ -13,35 +13,35 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundMask);
-        MovementController();
+        Move();
     }
-    public void MovementController()
+    public void Move()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         // calculate movement
         Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput);
-        if (rb.velocity.magnitude > 0.1f)
+        if (body.velocity.magnitude > 0.1f)
         {
             Vector3 target = Camera.main.transform.TransformDirection(moveDirection);
             target.y = 0f;
-            rb.velocity = target * speed;
+            body.velocity = target * speed;
         }
         else
         {
-            rb.velocity = Vector3.zero;
+            body.velocity = Vector3.zero;
         }
         if (isGrounded && Input.GetButtonDown("Jump"))
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 }

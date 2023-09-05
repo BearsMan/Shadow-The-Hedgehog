96c239dc059dba2305189e83d0a1cf26e1b63098
Darius Ronkowitz <dronkowitz@yahoo.com>
2023-09-05 17:12:20 -0400

    Added GamePadController Script
    Started basic control code with the use of a gamepad controller.

---
+++ GamePadController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class GamePadController : MonoBehaviour
+{
+    public GameObject controllerType;
+    public Sprite controllerIconColor;
+    public Sprite controllerIconSize;
+    public Sprite controllerIconScale;
+    public string controllerName;
+    public string controllerDescription;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

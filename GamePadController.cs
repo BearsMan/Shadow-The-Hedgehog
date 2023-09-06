15be819ed543932e82cd74ed0d2ddbfeba84d1a6
Darius Ronkowitz <dronkowitz@yahoo.com>
2023-09-06 11:28:17 -0400

    Updated GamePadController.cs
    Added Color GameObjects for gamepad controller color buttons.
    Will Update the game sprites

--- GamePadController.cs
+++ GamePadController.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 
 public class GamePadController : MonoBehaviour
 {
-    public GameObject controllerType;
+    public GameObject controller;
+    public Color controllerColor = Color.blue;
+    public Color gamepadColor = Color.red;
+    public Color gamepadColor2 = Color.yellow;
+    public Color gamepadColor3 = Color.green;
+    public static int numberOfControlButtons = 0;
+    public static bool isButtonPressed = false;
+    public static bool isButtonReleased = false;
+    public Rigidbody body;
+    private bool isGrounded = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame

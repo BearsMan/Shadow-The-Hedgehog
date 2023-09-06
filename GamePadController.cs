76b90b9c132bb1994d2fff72f9fffbfc700d97e5
Darius Ronkowitz <dronkowitz@yahoo.com>
2023-09-06 07:41:21 -0400

    Updated GamePadController.cs
    Removed a few sections of code because I will be looking for icons, sprites, etc.

--- GamePadController.cs
+++ GamePadController.cs
@@ -6,11 +6,6 @@ using UnityEngine;
 public class GamePadController : MonoBehaviour
 {
     public GameObject controllerType;
-    public Sprite controllerIconColor;
-    public Sprite controllerIconSize;
-    public Sprite controllerIconScale;
-    public string controllerName;
-    public string controllerDescription;
     // Start is called before the first frame update
     void Start()
     {

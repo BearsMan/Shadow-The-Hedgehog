abdd8d7a9f70a196836a66aefca76942bb31780d
Darius Ronkowitz <dronkowitz@yahoo.com>
2023-09-07 13:04:21 -0400

    Create CharacterAnimationController.cs
    Added Animations, and Basic Animation functions for each animation's character control.

---
+++ CharacterAnimationController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationController : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody body;
+    private bool isGrounded;
+    PlayerMovement playerMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        body = GetComponent<Rigidbody>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = playerMovement.isGrounded;
+        float horizontalSpeed = new Vector3(body.velocity.x, 0f, body.velocity.z).magnitude;
+        if (isGrounded)
+        {
+            anim.SetFloat("Speed", horizontalSpeed);
+        }
+    }
+}

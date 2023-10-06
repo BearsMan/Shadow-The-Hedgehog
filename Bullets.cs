a0e5bdd399b2a8d32b1821c7a572a289328ccbb9
Darius Ronkowitz <dronkowitz@yahoo.com>
2023-10-06 12:15:18 -0400

    `Updated Bullet.cs`
    Fixed some typos.

--- Bullets.cs
+++ Bullets.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class Bullets : MonoBehaviour
 {
+    // Checks how much speed, and range the weapon has, and uses.
     public float speed = 3f;
     public float range = 5f;
     // Start is called before the first frame update
     void Start()
     {
+        // Destroys the gameobject, and the range that the weapon fires at.
         Destroy(gameObject, range);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Fires the bullet in forward direction.
         transform.Translate(Vector3.right * speed * Time.deltaTime);
     }
 }

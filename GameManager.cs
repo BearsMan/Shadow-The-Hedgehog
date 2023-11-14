7df6606004ae1c857505c82495ff18b85177d2d6
Darius Ronkowitz <dronkowitz@yahoo.com>
2023-11-14 17:07:10 -0500

    `Updated GameManager.cs`
    Removed `Application.Quit` from the code.

--- GameManager.cs
+++ GameManager.cs
@@ -69,7 +69,7 @@ public class GameManager : MonoBehaviour
     }
     private void GameOver()
     {
-        Application.Quit(); // Quits the application.
+        
     }
     private void AddScore(int points)
     {

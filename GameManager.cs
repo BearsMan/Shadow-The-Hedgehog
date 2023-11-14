d7820224908694b7c91fe93562fc83fd35038633
Darius Ronkowitz <dronkowitz@yahoo.com>
2023-11-14 17:04:46 -0500

    `Updated GameManager.cs`
    Added timers in floats for the HUD UI.
    Added time variables, and more.

--- GameManager.cs
+++ GameManager.cs
@@ -7,29 +7,30 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public int rings = 5; // Collected at the start of the stage.
+    private float eplisedTime = 0f; // This show has time it has been since the stage has started.
     public float lightBar = 0f; // For all light attack types (blue bar).
     public float darkBar = 0f; // For evil attack types (red bar).
-    public int lives = 0;
-    public bool continueGame = false;
-    public bool restart = false;
+    public int lives = 0; // Checks how many lives that the character has.
     [Header("UI Elements")]
     public Slider lightBarSlider; // This is used for "Chaos Control" or "Chaos Spear" attacks. "Note." Must be filled before the attacks can be called out.
     public Slider darkBarSlider; // This is used for "Chaos Blast." "Note." Must be filled before the attack can be called out.
+    public TextMeshProUGUI livesUI; // This is used to show how lives the player has left.
     public TextMeshProUGUI scoreText;
-    private int Score;
-    public TextMeshProUGUI timer;
+    private int score;
+    private int timer;
+    public TextMeshProUGUI timerUI;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        HUDTimer();
     }
-    public void PlayerDamage(float damage)
+        public void PlayerDamage(float damage)
     {
         rings -= 10;
         darkBar += damage;
@@ -72,7 +73,21 @@ public class GameManager : MonoBehaviour
     }
     private void AddScore(int points)
     {
-        Score += points;
+        score += points;
+    }
+    private void HUDTimer()
+    {
+        eplisedTime += Time.deltaTime;
+        string formattedTime = FormatTime(eplisedTime);
+        timerUI.text = formattedTime;
+    }
+    string FormatTime(float time)
+    {
+        int minutes = Mathf.FloorToInt(time / 60f);
+        int seconds = Mathf.FloorToInt(time % 60);
+        int ms = Mathf.FloorToInt(time / 1000) % 1000;
+        return string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, ms);
+        
     }
 }
 

a57a0fab8f4ed577fd5cc32502e0169a4ed9c6b2
Darius Ronkowitz <dronkowitz@yahoo.com>
2024-09-15 11:02:42 -0400

    `Updated SaveLoad.cs`
    Added an option to save and load the game files on save and close the game via a keypress.

--- SaveLoad.cs
+++ SaveLoad.cs
@@ -4,5 +4,39 @@ using UnityEngine;
 public static class SaveLoad
 {
     public static int slotSelected;
+    public static SaveData SavedGame;
+
+    private static string SaveSlotName(int saveSlot)
+    {
+        return Application.persistentDataPath + "/Slot" + saveSlot + ".sav";
+    }
+    public static void SaveGame(SaveData dataToSave, int saveSlot)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(SaveSlotName(saveSlot));
+        bf.Serialize(file, dataToSave);
+        file.Close();
+        Load(saveSlot);
 
     }
+
+    public static bool Load(int saveSlot)
+    {
+        if (File.Exists(SaveSlotName(saveSlot)))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(SaveSlotName(saveSlot), FileMode.Open);
+
+            SavedGame = (SaveData)bf.Deserialize(file);
+            file.Close();
+            return true;
+        }
+        else return false;
+    }
+}
+
+[System.Serializable]
+public class SaveData
+{
+    public bool saveSelectedArea = false;
+}
